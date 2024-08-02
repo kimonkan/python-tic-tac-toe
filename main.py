321ae5fd143ac9d47e2d5f6fb4214c70d2bc079d
Kimon Kanellos <kimonkanellos@gmail.com>
2024-08-03 01:00:34 +0300

    Added basic game explanation

--- main.py
+++ main.py
@@ -1,2 +1,2 @@
 print("Hello there! This is a classic game of Tic-Tac-Toe.")
-print("This game was created to be played by 2 human players at the same computer.")
+print("This game was created to be played by 2 human players on the same computer.")
