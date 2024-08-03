c7a3d9bfaaf972248a0d8f544c28c77124ead0ee
Kimon Kanellos <kimonkanellos@gmail.com>
2024-08-03 14:43:44 +0300

    Fixed loop for choosing marker

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 print("Hello there! This is a classic game of Tic Tac Toe.")
 print("This game was created to be played by 2 human players on the same computer.")
 
-player1 = input("Player 1: Do you want to be X or O?")
+player1 = input("Player 1: Do you want to be X or O?").upper()
 player2 = ""
-while player1 != "X" or player1 != "O":
-    player1 = input("Player 1: Do you want to be X or O?")
+while player1 != "X" and player1 != "O":
+    player1 = input("Player 1: Do you want to be X or O? ").upper()
 if player1 == "X":
-    player2 == "O"
-    print("2 O")
+    player2 = "O"
+    print("\nPlayer 2 will be O.")
 else:
-    player2 == "X"
-    print("2 X")
-print("Player 1 will go first!")
+    player2 = "X"
+    print("\nPlayer 2 will be X.")
+print("\nPlayer 1 will go first!")
 
 
